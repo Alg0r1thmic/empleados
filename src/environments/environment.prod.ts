c5870358f5050016e809d00d19052b9382f70bf2
Raul <rquispeto@unsa.edu.pe>
2021-02-05 18:56:46 -0500

    feat: created project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDXVWLGEGlxrotrQOV3vLcKt3QmKHjYsYY",
+    authDomain: "flutter-26c9d.firebaseapp.com",
+    projectId: "flutter-26c9d",
+    storageBucket: "flutter-26c9d.appspot.com",
+    messagingSenderId: "1032967388831",
+    appId: "1:1032967388831:web:7bfab4aeb1405cb68b6caa",
+    measurementId: "G-Z95DQQCMQT"
+  }
+};
